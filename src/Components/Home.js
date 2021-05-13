808f78398d996bfeac8923dc8b37d4cfe123f284
ketanb56up <ketanb56up@gmail.com>
2021-05-13 23:05:18 +0530

    add to cart desgin done

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,7 +3,26 @@ import React from 'react';
 function Home() {
     return (
         <div>
-            <h3>Home Component</h3>
+            <div className="add-to-cart">
+                <img src="https://static.vecteezy.com/system/resources/thumbnails/000/496/007/small/Ecommerce_998.jpg" />
+            </div>
+            <h1>Home Component</h1>
+            <div className="cart-wrapper">
+                <div className="img-wrapper item">
+                    <img src="http://codeskulptor-demos.commondatastorage.googleapis.com/pang/FDqGDmc.png" />
+                </div>
+                <div className="text-wrapper item">
+                    <span>
+                        I-Phone
+                    </span>
+                    <span>
+                        Price: $1000.00
+                    </span>
+                </div>
+                <div className="btn-wrapper item">
+                    <button>Add To Cart</button>
+                </div>
+            </div>
         </div>
     );
 }
--- src/Containers/HomeContainer.js
+++ src/Containers/HomeContainer.js
@@ -1,3 +1,4 @@
 import Home from '../Components/Home';
 
+
 export default Home;
