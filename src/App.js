30f4c5ec344b68f4a4a4046f7f46f0a17c238750
Nisha <nisha@github.com>
2021-05-14 15:35:53 +0530

    header done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import HomeContainer from './Containers/HomeContainer';
+import HeaderContainer from './Containers/HeaderContainer'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <HeaderContainer />
       <HomeContainer />
     </div>
   );
